32a8523a0a98ef0a78d25917e9fee6414054519d
jonathanboss17 <jtboss47@gmail.com>
2019-10-11 11:27:58 -0400

    map stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { MapInteractionCSS } from 'react-map-interaction';
 import './App.css';
+import Map from './Components/Map'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Map />
+      </div>
+    )
+  }
 }
 
-export default App;
